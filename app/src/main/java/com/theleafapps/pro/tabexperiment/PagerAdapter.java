fb9b086de099d28c3d7cf7e7af271723e4fce6d7
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-08-30 21:51:01 +0530

    ViewPager Implemented PagerAdapter Created

---
+++ app/src/main/java/com/theleafapps/pro/tabexperiment/PagerAdapter.java
@@ -0,0 +1,32 @@
+package com.theleafapps.pro.tabexperiment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
